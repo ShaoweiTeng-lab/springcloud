04984b1b399e5d64a9881b9e704e49e4c4a5888d
Jason_Teng <sdonline13@gmail.com>
2023-11-14 11:50:12 +0800

    first update

--- userService/src/main/java/org/example/UserServiceApplication.java
+++ userService/src/main/java/org/example/UserServiceApplication.java
@@ -2,8 +2,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient//開啟服務註冊(可省)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
